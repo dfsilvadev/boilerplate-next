d0b956847722d1a589533a8205bb86e08bb505e2
Daniel Silva <dfsilva.dxp@gmail.com>
2023-09-15 18:37:54 -0300

    chore: add jest-styled-component.

--- jest.config.js
+++ jest.config.js
@@ -5,14 +5,21 @@ module.exports = {
   collectCoverageFrom: [
     'src/**/*.ts(x)?',
     '!src/app/**',
-    '!src/lib/registry.tsx'
+    '!src/lib/registry.tsx',
+    '!src/types/**',
+    '!src/styles/**'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
-  },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  },
+  // https://github.com/styled-components/styled-components/issues/4081
+  // v6 of styled-components doesn't inject styles in test environment
+  // we should to force it to use the browser version
+  moduleNameMapper: {
+    '^styled-components':
+      'styled-components/dist/styled-components.browser.cjs.js',
+    '^@/(.*)$': '<rootDir>/src/$1'
   }
 };
--- src/tests/components/main.test.tsx
+++ src/tests/components/main.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import Main from '@/components/Main';
 
 describe('<Main />', () => {
-  it('should render the heading', () => {
+  it('should render the heading and next logo', () => {
     const { container } = render(<Main />);
 
     const heading = screen.getByRole('heading', { name: /boilerplate/i });
@@ -15,5 +15,11 @@ describe('<Main />', () => {
     expect(nextLogo).toHaveAttribute('src', '/next.svg');
 
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render the background color currectly', () => {
+    const { container } = render(<Main />);
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#f2f3f5' });
   });
 });
